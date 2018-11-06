3635aadd55440b860ad032bcd0516f246734ebae
RajaiAbu-Saqer <rajaisaqer@gmail.com>
2018-11-06 12:40:28 +0200

    Updated

--- VerificationApp/app/src/main/java/com/test/verification/verificationapp/HelperClass.java
+++ VerificationApp/app/src/main/java/com/test/verification/verificationapp/HelperClass.java
@@ -1,19 +1,36 @@
 package com.test.verification.verificationapp;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+
+import com.test.verification.verificationapp.Sol.Verification_sol_Verification;
+
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.Web3jFactory;
 import org.web3j.protocol.http.HttpService;
 
 public class HelperClass {
+    Activity activity;
+    ProgressDialog progressDialog;
     public static Web3j web3 = Web3jFactory.build(new HttpService("https://rinkeby.infura.io/v3/203f4b27aa6a41c6958b6c8ff6f4d729"));// defaults to http://localhost:8545/
     public static String privateKey="4274b048585600a5732d24d055e5ca2ed6df5311b895d4ed6c1aea0019881021"
-            ,mainAddress="0xf21fe27e0721423577c46cd649970a15358a9726"
-            ,voterAddress="0xd1dcbeb004033102e5d3b5ba78f6cf75d0fe2590";
+            ,verificationAddress="0x07a4a97335ddce2951ecb9cc387e237c0785f3fc";
     public static Credentials credentials = Credentials.create(privateKey);
+    public static Verification_sol_Verification verification_sol_verification=null;
 
-//    public static Voters voters;
-//    public static Judgment judgment=null;
-//    public static Candidates candidates=null;
-//    public static MainContract mainContract=null;
+
+    public HelperClass(Activity act)
+    {
+        activity=act;
+    }
+    public ProgressDialog getProgressDialog(String title,String message) {
+        if(progressDialog==null)
+        progressDialog=new ProgressDialog(activity);
+        progressDialog.setTitle(title);
+        progressDialog.setMessage(message);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        return progressDialog;
+    }
 }
--- VerificationApp/app/src/main/java/com/test/verification/verificationapp/MainActivity.java
+++ VerificationApp/app/src/main/java/com/test/verification/verificationapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.test.verification.verificationapp;
 
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Vibrator;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.kenai.jffi.Main;
 
 import org.web3j.tx.Contract;
 import org.web3j.tx.ManagedTransaction;
@@ -21,11 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
     TextView name,age,sex,major,gpa,universityName,nationalID,dateOfBirth,placeOfBirth;
     FloatingActionButton scan;
+    String qrResultHash;
+    ProgressDialog progressDialog;
+    HelperClass helperClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        helperClass=new HelperClass(MainActivity.this);
 
 
         name=(TextView)findViewById(R.id.name);
@@ -79,14 +85,55 @@ public class MainActivity extends AppCompatActivity {
             if (result.getContents() == null) {
                 Log.d("MainActivity", "Cancelled scan");
             } else {
+
                 Vibrator vibrator=(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
                 vibrator.vibrate(100);
-                Toast.makeText(MainActivity.this, result.getContents(), Toast.LENGTH_SHORT).show();
-               // result.getContents();
+                qrResultHash=result.getContents();
+                new LongOperationGetInfo().execute("");
 
             }
         }
 
+
+    }
+
+    class LongOperationGetInfo extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if(HelperClass.verification_sol_verification ==null )
+                HelperClass.verification_sol_verification = HelperClass.verification_sol_verification.load(HelperClass.verificationAddress, HelperClass.web3, HelperClass.credentials, ManagedTransaction.GAS_PRICE, Contract.GAS_LIMIT);
+            progressDialog=helperClass.getProgressDialog("Please wait","Getting data");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                age.setText(HelperClass.verification_sol_verification.getAge(qrResultHash).send());
+                name.setText(HelperClass.verification_sol_verification.getFirstName(qrResultHash).send() +" "+HelperClass.verification_sol_verification.getLastName(qrResultHash).send());
+                sex.setText(HelperClass.verification_sol_verification.getSex(qrResultHash).send());
+                major.setText(HelperClass.verification_sol_verification.getMajor(qrResultHash).send());
+                gpa.setText(HelperClass.verification_sol_verification.getGPA(qrResultHash).send());
+                universityName.setText(HelperClass.verification_sol_verification.getUniversityName(qrResultHash).send());
+                nationalID.setText(HelperClass.verification_sol_verification.getNationalID(qrResultHash).send());
+                dateOfBirth.setText(HelperClass.verification_sol_verification.getDateOfBirth(qrResultHash).send());
+                placeOfBirth.setText(HelperClass.verification_sol_verification.getPlaceOfBirth(qrResultHash).send());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressDialog.dismiss();
+
+        }
 
     }
 
